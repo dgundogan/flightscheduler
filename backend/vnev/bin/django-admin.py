fc56f1ad875be2d460fb34d43431610d2c76d8fa
dursun gündoğan <>
2020-03-21 23:07:10 +0300

    init

---
+++ backend/vnev/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dursungundogan/Documents/django-project/django_angular/backend/vnev/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
